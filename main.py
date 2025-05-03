8bb878d9e7092ac6c7a5f38378cda6aa3465bff8
YaelPerez <64277661+YaelPerez@users.noreply.github.com>
2025-05-03 17:07:56 -0600

    feat: endpoint para generar y descargar archivo Excel

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi.responses import StreamingResponse
+import pandas as pd
+from io import BytesIO
 
 app = FastAPI()
 
-@app.get("/sumar")
-def sumar():
-    resultado = 2 + 3
-    return JSONResponse(content={"mensaje": f"La suma de 2 y 3 es: {resultado}"})
+@app.get("/generar_excel")
+def generar_excel():
+    df = pd.DataFrame({
+        "Nombre": ["Yael", "Verona", "Tito"],
+        "Edad": [30, 5, 3],
+        "Ciudad": ["Toluca", "Toluca", "Toluca"]
+    })
+
+    output = BytesIO()
+    with pd.ExcelWriter(output, engine='openpyxl') as writer:
+        df.to_excel(writer, index=False, sheet_name='Datos')
+    output.seek(0)
+
+    return StreamingResponse(
+        output,
+        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        headers={"Content-Disposition": "attachment; filename=archivo_generado.xlsx"}
+    )
